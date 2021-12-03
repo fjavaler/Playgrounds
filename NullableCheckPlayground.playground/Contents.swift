37ca6d93ab805ad6237df32e6e23854c60cc7071
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-02 18:25:44 -0700

    commit

---
+++ NullableCheckPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Defaults to nil.
+var myBool: Bool?
+
+print(myBool)
+
+if myBool == false {
+  print(myBool)
+} else {
+  print(myBool)
+}
+
+if !myBool {
+  print(myBool)
+} else {
+  print(myBool)
+}
